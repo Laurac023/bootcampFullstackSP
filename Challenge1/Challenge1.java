8f35484a002861c88d06a93d787d89d2af248d92
Laura <laurisco023@gmail.com>
2022-11-14 17:24:15 -0500

    Metodo booleano

--- Challenge1/Challenge1.java
+++ Challenge1/Challenge1.java
@@ -3,20 +3,19 @@ package Challenge1;
 import java.util.*;
 
 public class Challenge1 {
+	static Scanner sc = new Scanner(System.in);
+	static List<String> lista = new ArrayList<String>();	
 	public static void main(String[]args){
 		int longitud,tipo, cantidadCadenas;
-		String numeros, A, cadena="";
-		longitud = 8;
-		A = "A";
-		
-		Scanner sc = new Scanner(System.in);
+		String numeros, cadena="";
+		longitud = 8;		
 		System.out.print("Cuantas cadenas quiere generar:  ");
 		cantidadCadenas = sc.nextInt();
 		
-		List<String> lista = new ArrayList<String>();
+
 		for(int x = 0; x < cantidadCadenas; x++) {
 			int num = x+1;
-			System.out.print("Elija el tipo de cadena número " + num + " de texto: Tipo A = 1  o B = 2 ");
+			System.out.print("Elija el tipo de la cadena número " + num + " de texto: Tipo A = 1  o B = 2 ");
 			tipo = sc.nextInt();
 
 			if(tipo == 1) {
@@ -35,13 +34,23 @@ public class Challenge1 {
 				lista.add(cadena);
 			}
 		}
+		int size = lista.size();
 		System.out.println("La lista es " + lista);
+		System.out.println("La lista es " + size);
 	}
 	//metodo para ver si la cadena esta en la lista o no
-//	public boolean lista(String lista) {
-//		boolean existe = false;
-//		if(lista == ) {
-//			existe = true;
+//	public boolean verificar() {
+//		boolean existe;
+//		String insert, posicion;
+//		System.out.print("Ingrese su cadena de 10 digitos: ");
+//		insert = sc.nextLine();
+//		for (int y = 1; y < lista.length(); y++) {
+//			posicion = lista[i];
+//			if(insert.equals(posicion){
+//				existe = false;
+//			}else {
+//				existe = true;
+//			}
 //		}
 //		System.out.println("La cadena es: " + existe);
 //		return existe;
