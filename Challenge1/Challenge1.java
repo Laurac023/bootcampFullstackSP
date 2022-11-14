256a3d105b8d13fce5f4b9366663195391739ce9
Laura <laurisco023@gmail.com>
2022-11-14 18:08:15 -0500

    Metodo booleano

--- Challenge1/Challenge1.java
+++ Challenge1/Challenge1.java
@@ -3,43 +3,44 @@ package Challenge1;
 import java.util.*;
 
 public class Challenge1 {
+	
 	static Scanner sc = new Scanner(System.in);
 	static List<String> lista = new ArrayList<String>();	
-	static boolean existe = verificar();
 	public static void main(String[]args){
-
-		int longitud,tipo, cantidadCadenas;
-		String numeros, cadena="";
-		longitud = 8;		
+		int length,type,amountCadenas;
+		String numbers, cadena="";
+		length = 8;		
 		System.out.print("Cuantas cadenas quiere generar:  ");
-		cantidadCadenas = sc.nextInt();
-		System.out.println("La cadena es: " + existe);
-		System.out.println("La lista es " + lista);
+		amountCadenas = sc.nextInt();
 		
-		for(int x = 0; x < cantidadCadenas; x++) {
+
+		for(int x = 0; x < amountCadenas; x++) {
 			int num = x+1;
 			System.out.print("Elija el tipo de la cadena número " + num + " de texto: Tipo A = 1  o B = 2 ");
-			tipo = sc.nextInt();
+			type = sc.nextInt();
 
-			if(tipo == 1) {
+			if(type == 1) {
 				cadena = "54";
-				for(int i = 1; i<=longitud;i++) {
-					numeros = String.valueOf(Double.valueOf(Math.random()*10).intValue());
-					cadena += numeros;
+				for(int i = 1; i<=length;i++) {
+					numbers = String.valueOf(Double.valueOf(Math.random()*10).intValue());
+					cadena += numbers;
 				}
 				lista.add(cadena);
 			}else {
 				cadena = "08";
-				for(int i = 1; i<=longitud;i++) {
-					numeros = String.valueOf(Double.valueOf(Math.random()*10).intValue());
-					cadena += numeros;
+				for(int i = 1; i<=length;i++) {
+					numbers = String.valueOf(Double.valueOf(Math.random()*10).intValue());
+					cadena += numbers;
 				}
 				lista.add(cadena);
 			}
 		}
+		System.out.println("La lista es " + lista);
+		System.out.println("La lista es " + lista.get(1));
 	}
 	//metodo para ver si la cadena esta en la lista o no
-	public static boolean verificar() {
+	public boolean verify() {
+		boolean existe = false;
 		String insert, posicion;
 		int size = lista.size();
 		System.out.print("Ingrese su cadena de 10 digitos: ");
@@ -52,6 +53,7 @@ public class Challenge1 {
 				existe = true;
 			}
 		}
+		System.out.println("La cadena es: " + existe);
 		return existe;
 	}
 }
