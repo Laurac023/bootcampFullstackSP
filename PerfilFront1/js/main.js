d65f7b21400745acc371c7809c2fdf89f314a5c0
Laura <laurisco023@gmail.com>
2022-11-30 16:05:43 -0500

    Perfil front done

---
+++ PerfilFront1/js/main.js
@@ -0,0 +1,31 @@
+function card1() {
+    debugger;
+    var card1 = document.getElementById('card1');
+    if (card1.style.display === 'none') {
+        card1.style.display = 'block';
+    }
+    else {
+      card1.style.display = 'none';
+    }
+  }
+
+  function card2() {
+    debugger;
+    var card2 = document.getElementById('card2');
+    if (card2.style.display === 'none') {
+        card2.style.display = 'block';
+    }
+    else {
+      card2.style.display = 'none';
+    }
+  }
+  function card3() {
+    debugger;
+    var card3 = document.getElementById('card3');
+    if (card3.style.display === 'none') {
+        card3.style.display = 'block';
+    }
+    else {
+      card3.style.display = 'none';
+    }
+  }
